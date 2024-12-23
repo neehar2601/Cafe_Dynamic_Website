4304a473f3d83050b5707d049d465412aae18bdf
bharathac5775 <118104583+bharathac5775@users.noreply.github.com>
2024-12-23 23:49:36 +0530

    Update getAppParameters.php

--- mompopcafe/getAppParameters.php
+++ mompopcafe/getAppParameters.php
@@ -4,7 +4,7 @@
   $showServerInfo = "false";
   $timeZone = "America/New_York";
   $currency = "$";
-  $db_url = "mymysql";
+  $db_url = "cafeapp-argo-rollout-mysql";
   $db_name = "mom_pop_db";
   $db_user = "root";
   $db_password = "Msois@123";
