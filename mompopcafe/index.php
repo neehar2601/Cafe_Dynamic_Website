30a82d1e926393ae5fe30c63b2b9c2e4ed2f1fcb
bharathac5775 <118104583+bharathac5775@users.noreply.github.com>
2025-02-25 23:30:38 +0530

    Update index.php

--- mompopcafe/index.php
+++ mompopcafe/index.php
@@ -10,7 +10,7 @@
 
 	<div id="header" class="mainHeader">
 		<hr>
-		<div class="center">cloud compute msis Mom &amp; Pop Caf&eacute;</div>
+		<div class="center">MSIS Mom &amp; Pop Caf&eacute;</div>
 	</div>
 	<br>
 	<?php
